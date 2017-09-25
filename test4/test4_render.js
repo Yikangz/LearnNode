a8ff33df7d43c0ecdcfae003fc968fb3961d024f
YiKangz <229048466@qq.com>
2017-09-25 16:43:03 +0800

    根据后缀传入类型,返出相应的mime值

--- test4/test4_render.js
+++ test4/test4_render.js
@@ -5,23 +5,23 @@ var path = require('path');
 
 http.createServer(function (req, res) {
     //console.log(req);
-    
+
     var pathname = url.parse(req.url).pathname;
 
     if (req.url == '/favicon.ico') {
         return;
     }
-   
+
     if (pathname == '/') {
         pathname = 'index.html';
     }
-   
+
     var extname = path.extname(pathname);
 
     console.log(extname);
 
     fs.readFile('./fileDir/' + pathname, function (err, data) {
-        if (err) {
+         if (err) {
 
             //读取错误页面
             fs.readFile('./fileDir/404.html', function (err, data) {
@@ -30,22 +30,47 @@ http.createServer(function (req, res) {
             });
             return;
         } 
-        var mime = getMime(extname);
-        //console.log(mime);
-        res.writeHead(200, { "content-type": mime });
-        res.end(data);
+        
+        getMime(extname, function (mime) {
+            console.log('123123' + mime);
+            res.writeHead(200, { "content-type": mime });
+            res.end(data);
+        });
+
+       
+
     });
 
-  
+
 }).listen(3000, '127.0.0.1');
 
-function getMime(extname){
-    switch(extname){
+function getMime(extname, callback) {
+
+    var datas = fs.readFile('./fileDir/mime.json', 'utf8', function (err, data) {
+
+        if (err) {
+            //读取错误页面
+            res.writeHead('404', { 'content-type': 'text/html;charset=utf8' })
+            res.end('404,没有找到mime!!');
+            return;
+        }
+
+        var result = JSON.parse(data);
+        var mime = result[extname] || 'text/plain';
+        console.log('111111' + mime);
+        callback(mime);
+    });
+
+
+
+
+
+    /*  switch(extname){
         case ".html" :
             return "text/html";
             break;
         case ".img" :
             return "image/jpg";
             break;
-    }
+    }  */
 }
