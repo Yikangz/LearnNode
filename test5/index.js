3ec4d7521e65d2da0b52d6297cc6fa15cb4a3d37
Tony <229048466@qq.com>
2017-09-25 22:18:47 +0800

    了解包与package.json的关系

--- test5/index.js
+++ test5/index.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var abc = require('abc');
 
-http.createServer(function(err,data){
+http.createServer(function(req,res){
     
+	if (req.url == '/favicon.ico'){
+		return ;
+	}
 
+    res.writeHead(200,{'content-type':'text/xhtml;charset=utf8'})
+	console.log('abc:'+abc.run)
+	res.end('HelloWorld');
 
 
 }).listen(3000,'127.0.0.1');
